ef1b58537798c2a1c1a54375ea82b5b43eb147e2
ronen <narkisr@gmail.com>
2017-01-11 00:06:46 +1100

    latest depdencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject self-build "0.0.9"
 
-  :description 
+  :description
   "self-build is a simple build server currently focused on simplicity and easy setup,
    its main goal is to enable a continues build by just running lein self-build jobs.edn"
 
@@ -10,34 +10,35 @@
     :name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"
   }
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-jgit "0.6.3"] 
-                 [com.taoensso/timbre "3.1.1"]
-                 [org.clojure/core.incubator "0.1.3"]
-                 [substantiation "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-jgit "0.8.9"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [org.clojure/core.incubator "0.1.4"]
+                 [substantiation "0.3.1"]
                  [me.raynes/fs "1.4.6"]
                  [ruiyun/tools.timer "1.0.1"]
                  [me.raynes/conch "0.8.0"]
                  [com.draines/postal "1.11.1"]
-                 [formation "0.0.1"]
+                 [formation "0.1.0"]
+                 [com.fzakaria/slf4j-timbre "0.2"]
                  [org.clojure/tools.reader "0.8.13"]]
 
-  :plugins [[lein-tag "0.1.0"] [lein-set-version "0.3.0"] 
-            [lein-ancient "0.6.2" :exclusions [rewrite-clj]] [lein-cljfmt "0.1.9"]]
+  :plugins [[lein-tag "0.1.0"] [lein-set-version "0.3.0"]
+            [lein-ancient "0.6.10" :exclusions [rewrite-clj]] [lein-cljfmt "0.1.9"]]
 
   :profiles {
-    :dev { 
+    :dev {
       :set-version {
-        :updates [ 
+        :updates [
           {:path "README.md" :search-regex #"\"\d+\.\d+\.\d+\""}
         ]
       }
     }
   }
- 
+
   :aliases {"self-build" ["trampoline" "run" "fixtures/jobs.edn"] }
 
   :signing  {:gpg-key "narkisr@gmail.com"}
-  
+
   :main self-build.core
-  )
+)
--- src/self_build/core.clj
+++ src/self_build/core.clj
@@ -1,6 +1,6 @@
 (ns self-build.core
   (:gen-class true)
-  (:require 
+  (:require
    [subs.core :refer (validate! validation when-not-nil every-kv)]
    [formation.core :as form]
    [me.raynes.fs :as fs]
@@ -15,8 +15,8 @@
    [me.raynes.conch.low-level :as low]
    [me.raynes.conch.low-level :as sh]
    [clj-jgit.porcelain :as g :refer (with-identity git-clone-full git-checkout git-branch-current)])
-  (:import 
-    org.eclipse.jgit.api.errors.CheckoutConflictException 
+  (:import
+    org.eclipse.jgit.api.errors.CheckoutConflictException
     clojure.lang.ExceptionInfo))
 
 (timbre/refer-timbre)
@@ -25,20 +25,20 @@
 (def errors (atom {}))
 (def statuses (atom {}))
 
-(defn log-proc 
+(defn log-proc
   "logging out stdout/stderr and collecting both of them to errors atom"
-  [id out proc] 
+  [id out proc]
   (swap! errors (fn [m] (assoc m id (str (m id) out "\n"))))
   (info out))
 
 (defn- options [id args]
   (let [defaults {:verbose true :timeout 60 :out (partial log-proc id) :err (partial log-proc id)}]
     (if (map? (last args))
-      [(butlast args) (merge defaults (last args))] 
+      [(butlast args) (merge defaults (last args))]
       [args defaults])
     ))
 
-(defn sh- 
+(defn sh-
   "Runs a command localy and logs its output streams"
   [id cmd args]
   (info cmd (join " " args))
@@ -48,42 +48,42 @@
         :timeout (throw (ExceptionInfo. (<< "timed out while executing: ~{cmd}") opts))
         0 nil)
       (catch Throwable e
-        (throw (ExceptionInfo. (.getMessage e) {:id id}))) 
+        (throw (ExceptionInfo. (.getMessage e) {:id id})))
       )))
 
 
-(defn handle-success 
+(defn handle-success
   [name smtp mail]
   (let [{:keys [status date]} (@statuses name)]
     (when (= status :fail)
       (send-message smtp
-         (merge mail 
-            {:subject (<< "Building ~{name} restored to succeful!") 
+         (merge mail
+            {:subject (<< "Building ~{name} restored to succeful!")
              :body (<< "The build which failed on ~{date} is now working")}))))
   (swap! statuses assoc name {:status :success :date (java.util.Date.)}))
 
-(defn handle-error 
+(defn handle-error
   [name smtp mail e]
   (swap! statuses assoc name {:status :fail :date (java.util.Date.)})
   (send-message smtp
-         (merge mail 
-            {:subject (<< "Building ~{name} failed!") 
+         (merge mail
+            {:subject (<< "Building ~{name} failed!")
              :body (<< "failed to build ~{name} due to:\n ~(.getMessage e) ~(@errors name)")}))
   (swap! errors assoc name ""))
 
-(defn build 
-  "runs build steps" 
+(defn build
+  "runs build steps"
   [{:keys [steps target name] :as job} {:keys [smtp mail] :as ctx}]
   (info "Starting to build" name)
-  (try 
-    (doseq [{:keys [cmd args timeout] :or {timeout 60}} steps] 
+  (try
+    (doseq [{:keys [cmd args timeout] :or {timeout 60}} steps]
       (sh- name cmd (conj args {:dir target :timeout timeout})))
     (info "Finished building" name)
     (handle-success name smtp mail)
     (catch ExceptionInfo e (handle-error name smtp mail e))))
 
-(defn initialize 
-  "init build" 
+(defn initialize
+  "init build"
   [{:keys [branch repo target] :as job} {:keys [ssh-key] :as ctx}]
   (when-not (.exists (file target))
     (with-identity {:ssh-prvkey ssh-key}
@@ -96,61 +96,61 @@
       (git-checkout repo branch true true (<< "origin/~{branch}"))))
   (build job ctx))
 
-(defn re-initialize 
-  "re-checkout code descarding existing source (mainly useful on merge conflicts)." 
+(defn re-initialize
+  "re-checkout code descarding existing source (mainly useful on merge conflicts)."
   [{:keys [target name] :as job} ctx]
   (fs/delete-dir target)
   (initialize job ctx))
 
 (defn safe-merge [repo c]
-   (try 
+   (try
      (when-not (.isSuccessful (.getMergeStatus (g/git-merge repo c)))
        (throw (ExceptionInfo. (<< "Failed to merge code on ~{name}") {:merged-failed true})))
       (catch CheckoutConflictException e
         (throw (ExceptionInfo. (<< "Failed to checkout code on ~(.getConflictingPaths e)") {:merged-failed true})))))
 
-(defn periodic-check 
+(defn periodic-check
   "periodical check of build status"
   [{:keys [target name clear-merge-fail] :as job} {:keys [ssh-key] :as ctx}]
   (fn []
     (info "Checking build status")
     (with-identity {:ssh-prvkey ssh-key}
       (try
-        (let [repo (g/load-repo target) 
+        (let [repo (g/load-repo target)
               {:keys [trackingRefUpdates advertisedRefs]} (bean (g/git-fetch repo))]
           (when (> (.size trackingRefUpdates) 0)
             (doseq [c advertisedRefs] (safe-merge repo c))
             (info "Change detected running the build:")
             (build job ctx)))
-        (catch ExceptionInfo e 
+        (catch ExceptionInfo e
           (when (and clear-merge-fail (:merged-failed (.getData e)))
             (warn "Merge failed, clearing source and starting from scratch")
             (re-initialize job ctx)))
         (catch Throwable e (error e))))))
 
-(defn run-jobs 
-  "run all build jobs" 
+(defn run-jobs
+  "run all build jobs"
   [jobs ctx]
-  (doseq [{:keys [poll name] :as job} jobs] 
+  (doseq [{:keys [poll name] :as job} jobs]
     (info "Setting up job" name)
     (initialize job ctx)
     (run-task! (periodic-check job ctx) :period poll)))
 
 (def ctx-v
-  {:ctx {:ssh-key #{:required :String} 
-         :smtp {:host #{:required :String} :user #{:required :String} 
+  {:ctx {:ssh-key #{:required :String}
+         :smtp {:host #{:required :String} :user #{:required :String}
                 :pass #{:required :String} :ssl #{:required :Keyword} }
          :mail {:from #{:required :String} :to #{:required :String}}}})
 
-(defn ctx-validation 
-  "Validating ctx" 
+(defn ctx-validation
+  "Validating ctx"
   [ctx]
   (validate! ctx ctx-v))
 
 (defn locknload
-  "load jobs and run them" 
+  "load jobs and run them"
   [f]
-  (let [{:keys [jobs]} (edn/read-string (slurp f)) 
+  (let [{:keys [jobs]} (edn/read-string (slurp f))
         {:keys [ctx]} (form/config "self-build" ctx-validation)]
     (run-jobs jobs ctx)))
 
